22d8cf188054bf12412d96c4b4548c55493dd477
Letania Ferreira <letaniaferreira@gmail.com>
2019-10-27 12:36:07 -0700

    Add oficinas

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main():
     return render_template('front_page.html')
 
+@app.route('/oficinas')
+def oficinas():
+    return render_template('oficinas.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.jinja_env.auto_reload = app.debug
